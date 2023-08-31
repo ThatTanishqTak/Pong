9eea8e6602292891f24dca5477d5ca2f525fee4c
ThatTanishqTak <tanishqtak3@gmail.com>
2023-08-31 18:05:17 +0530

    Ball-wall collision is working

--- Pong/ball.cpp
+++ Pong/ball.cpp
@@ -12,15 +12,13 @@ Ball::~Ball() { unload(); }
 
 void Ball::update()
 {
-	ballPos.x += ballMoveSpeed.x * GetFrameTime();
+	//ballPos.x += ballMoveSpeed.x * GetFrameTime();
 	ballPos.y += ballMoveSpeed.y * GetFrameTime();
 
-	if (ballPos.x + radius >= 1080.0f) { grap_ball_obj->playerScore++; }
-	if (ballPos.x - radius <= 0.0f) { grap_ball_obj->oppoScore++; }
-	if (ballPos.y + radius >= 720.0f + radius) { ballMoveSpeed.x *= -1.0f; }
+	//if (ballPos.x + radius >= 1080.0f) { grap_ball_obj->playerScore++; }
+	//if (ballPos.x - radius <= 0.0f) { grap_ball_obj->oppoScore++; }
+	if (ballPos.y + radius >= 720.0f) { ballMoveSpeed.y *= -1.0f; }
 	if (ballPos.y - radius <= 0.0f) { ballMoveSpeed.y *= -1.0f; }
-
-	if (ballPos.x + radius <= play_ball_obj->playerPos.y && ballPos.x + radius>= play_ball_obj->playerPos.y + play_ball_obj->playerSize.y) { ballMoveSpeed.x *= -1.0f; }
 }
 
 void Ball::render() { DrawCircle(static_cast<int>(ballPos.x), static_cast<int>(ballPos.y), radius, WHITE); }
@@ -28,7 +26,7 @@ void Ball::render() { DrawCircle(static_cast<int>(ballPos.x), static_cast<int>(b
 void Ball::initVariables()
 {
 	radius = 20.0f;
-	ballMoveSpeed = { -500.0f, 0.0f };
+	ballMoveSpeed = { -500.0f, -500.0f };
 	ballPos = { 540.0f, 360.0f };
 }
 
